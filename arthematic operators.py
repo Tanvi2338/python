e26a97a1cde2fc5a7656d32e0db16aa879a42154
Tanvi2338 <venkatachalamtanvi@gmail.com>
2026-09-03 12:51:30 +0530

    Add files via upload

---
+++ arthematic operators.py
@@ -0,0 +1,13 @@
+#arthematic operators
+
+a=10
+b=3
+
+print("Addition:", a + b)
+print("Subtaction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
+    
